d78cd0abc41fe9acd860539b69b31802c7dd1fc3
lahit1 <naifcanbasci@gmail.com>
2026-03-29 00:30:59 +0300

    Initial Commit

---
+++ android/src/main/java/ieee/cs/isik/platformergaeme/android/AndroidLauncher.java
@@ -0,0 +1,18 @@
+package ieee.cs.isik.platformergaeme.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import ieee.cs.isik.platformergaeme.Main;
+
+/** Launches the Android application. */
+public class AndroidLauncher extends AndroidApplication {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AndroidApplicationConfiguration configuration = new AndroidApplicationConfiguration();
+        configuration.useImmersiveMode = true; // Recommended, but not required.
+        initialize(new Main(), configuration);
+    }
+}
