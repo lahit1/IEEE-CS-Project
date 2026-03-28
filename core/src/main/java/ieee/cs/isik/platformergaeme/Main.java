670da32767f6e96f2605f77c03f7873774e5f54b
lahit1 <naifcanbasci@gmail.com>
2026-03-29 00:56:00 +0300

    Simple black screen

--- core/src/main/java/ieee/cs/isik/platformergaeme/Main.java
+++ core/src/main/java/ieee/cs/isik/platformergaeme/Main.java
@@ -1,32 +1,39 @@
 package ieee.cs.isik.platformergaeme;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
-/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+/**
+ * Don't forget we're using OpenGL 2.0
+ **/
 public class Main extends ApplicationAdapter {
-    private SpriteBatch batch;
-    private Texture image;
-
     @Override
     public void create() {
-        batch = new SpriteBatch();
-        image = new Texture("libgdx.png");
+        // Set process name (Name of the window in desktop/laptop or name of the app in mobile)
+        Gdx.graphics.setTitle("IEEE CS Ekibi Mario");
+
+        // Set window size
+        Gdx.graphics.setWindowedMode(640, 480);
+
+        // Set default background color
+        Gdx.gl20.glClearColor(0, 0, 0, 1);
     }
 
     @Override
     public void render() {
-        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
-        batch.begin();
-        batch.draw(image, 140, 210);
-        batch.end();
+        /*
+         * Clear previous screen
+         * This will paint entire screen to the default color that we decided in create() with Gdx.gl20.glClearColor function
+         */
+        Gdx.gl20.glClear(GL20.GL_COLOR_BUFFER_BIT);
     }
 
     @Override
     public void dispose() {
-        batch.dispose();
-        image.dispose();
+        // Nothing to dispose
     }
 }
